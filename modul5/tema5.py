37a818a3a557296c114ed991ced223cee922d6b2
Emanuel Craciun <emcraciu@cisco.com>
2020-07-17 12:01:19 +0300

    teme modul 4/5

---
+++ modul5/tema5.py
@@ -0,0 +1,9 @@
+# Creati o clasa numita Triunghi.
+#  - obiectele create cu aceasta clasa au 3 variabile de instanta are reprezinta unghiurile triungiului si au valoarea
+# initiala de 60 de grade
+#  - clasa are o metoda numita modifica_unghi si primeste ca argumente unghiul si numarul de grade cu care trebuie
+#  modificat ca si int
+# - deoarece suma ungiurilor intr-un triungi trebe sa fie 180 grade metoda de modificare a unui unghiului trebuie sa
+# ajusteze celelalte doua variabile reprezentand ungiuri cu valori egale astfel ca suma celor 3 variabile sa ramana 180
+# - Daca modificarea ungiului are ca rezultat un unghi in afara intervalului deschis (0-180) metoda va ridica exceptia
+# AttributeError
